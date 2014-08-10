b8b608838c094398de9ce8779aebb704c95250ed
Md Alam <mdalam1218@gmail.com>
2014-08-10 00:46:27 -0400

    it's not working

---
+++ DivitionProgramInJava.java
@@ -0,0 +1,14 @@
+package com.all.eight.project;
+
+public class DivitionProgramInJava {
+
+	public static void main(String[] args) {
+		int firstNumber=2;
+		int secandNumber=8;
+		int d;
+		d=firstNumber/secandNumber;
+		System.out.println(d);
+
+	}
+
+}
