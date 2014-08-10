b1bb46846850a6d5ee035a6b86a5e0ae8e2e80f5
Md Alam <mdalam1218@gmail.com>
2014-08-10 01:32:24 -0400

    print . number one by one

---
+++ PrintNumber.java
@@ -0,0 +1,34 @@
+package com.all.eight.project;
+
+public class PrintNumber {
+
+	public static void main(String[] args) {
+	      int number = 5;
+	      
+	      // Using nested-if
+	      if (number == 1) {
+	    	  System.out.println("ONE");
+	      } else if (number == 2) {
+	    	  System.out.println("TWO");
+	      } else if (number==3) {
+	    	  System.out.println("THRE");
+	    	  }
+	      
+	       
+	 
+	     
+	      switch(number) 
+	      {
+	         case 1: System.out.println("ONE"); break;
+	         case 2: System.out.println("TWO"); break;
+	         case 3: System.out.println("THREE"); break;
+	         default: System.out.println("OTHER");
+	       
+	      }
+	   }
+}
+	
+
+	
+
+
