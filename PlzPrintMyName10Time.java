0e5c0e021a30ca0f17b45ad32ed754d90998ec29
Md Alam <mdalam1218@gmail.com>
2014-08-10 01:47:04 -0400

    print sum thing 10  time

---
+++ PlzPrintMyName10Time.java
@@ -0,0 +1,25 @@
+package com.all.eight.project;
+
+public class PlzPrintMyName10Time {
+
+	public static void main(String[] args) {
+
+        int i = 0;
+        int j = 4;
+
+   for(i = 1; i <= j; i++){
+
+          System.out.println("MAHBUB. Ima a BANGALI");
+
+
+   }
+    
+
+
+}   
+
+
+
+	}
+
+
